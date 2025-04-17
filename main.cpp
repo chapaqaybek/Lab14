9d2f0c289a3aeef88b927d8e994b7b5bd903b07a
chapaqaybek <s.numonov@newuu.uz>
2025-04-17 10:41:20 +0500

    initial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,74 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
+    // Problem 1
+    // ofstream file("output.txt");
+    //     file << "Hello, File";
+    //     file.close();
+    //     return 0;
+
+
+    // Problem 2
+    // ifstream file("greetings.txt");
+    // string line;
+    //
+    // while (getline(file, line)) {
+    //     cout << line << endl;
+    // }
+    //
+    // file.close();
+    // return 0;
+
+    // Problem 3
+    // string name;
+    // int age;
+    //
+    // cout << "Enter your name: ";
+    // cin >> name;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    //
+    // ofstream file("user.txt");
+    // file << name << " " << age;
+    // file.close();
+    //
+    // return 0;
+
+    // Problem 4
+    //     ifstream file("data.txt");
+    //     string line;
+    //
+    //     while (getline(file, line)) {
+    //         cout << line << endl;
+    //     }
+    //
+    //     file.close();
+    //     return 0;
+
+    // // Problem 5
+    // ifstream source("source.txt");
+    // ofstream target("target.txt");
+    //
+    // string line;
+    //
+    // while (getline(source, line)) {
+    //     target << line << endl;
+    // }
+    //
+    // source.close();
+    // target.close();
+    //
+    // return 0;
+
+    // Problem 6
+
+
+
+
+
+
+
 
 }
